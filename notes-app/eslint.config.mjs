07f49ae9a9c19b673ab89e4be98e82bb3979404f
amirlasalle <mirolasalle@gmail.com>
2024-12-13 10:00:54 -0500

    working on deployment e.

--- notes-app/eslint.config.mjs
+++ notes-app/eslint.config.mjs
@@ -1,45 +1,18 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-// ];
-
-// export default eslintConfig;
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-// Resolving __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Create FlatCompat instance
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
 
-// Define the ESLint configuration
 const eslintConfig = [
-  // Extending Next.js core and typescript rules
   ...compat.config({
-    extends: [
-      "next/core-web-vitals", // Core web vitals rules for Next.js
-      "next/typescript", // TypeScript rules for Next.js
-    ],
+    extends: ['next'],
     rules: {
-      'react/no-unescaped-entities': 'off', // Disable specific rule
-      '@next/next/no-page-custom-font': 'off', // Disable specific rule
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
   }),
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
